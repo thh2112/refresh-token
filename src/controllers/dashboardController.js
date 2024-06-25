2f546909593ee728ba624f446f8d3fe0bce7b344
Kie Tran - Hoang Tran <kie.tran@lfglobaltech.com>
2024-06-25 09:35:32 +0700

    chore: fix structure project

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -2,7 +2,7 @@ import { StatusCodes } from 'http-status-codes'
 
 const access = async (req, res) => {
   try {
-    const user = { email: 'trungquandev.official@gmail.com' }
+    const user = { email: 'huyhoangtest@gmail' }
 
     res.status(StatusCodes.OK).json(user)
   } catch (error) {
